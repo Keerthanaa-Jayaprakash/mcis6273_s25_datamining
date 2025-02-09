18675e6a8a59f78dfb4571496785b10795b4e7da
Keerthanaa Jayaprakash <keerthanaa.jayaprakashsekar@gmail.com>
2025-02-09 02:44:04 +0000

    Added markdown_practice.md and python_practice.py

---
+++ hw0/markdown_practice.md
@@ -0,0 +1,18 @@
+# Heading 1
+This is a markdown file. I am using it for practicing the basics of markdown format.
+## Heading 2
+This is a part of HW 0 from data mining class
+### Heading 3
+Let's see what I learn 
+
+
+- Python
+- Data Mining
+    - Supervised learning
+    - Unsupervised learning
+
+
+*Trying out the italicized style*
+
+**Trying out the bold style**
+
---
+++ hw0/python_practice.py
@@ -0,0 +1,15 @@
+# MCIS 6273 - Data Mining
+# Homework 0 - Python Practice in JupyterLab
+
+print("Hello, Jupyter! This is my first Python program.")
+
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = [x**2 for x in numbers]
+
+print(squared_numbers)
+
+def greet(name):
+    return f"Hello, {name}! Welcome to Data Mining."
+
+print("\n💡 Function Example:")
+print(greet("Keerthanaa"))
